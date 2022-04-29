2878db16d0818a61170ccc383b2219798a84347c
dosadczuk <damian.osadczuk@gmail.com>
2022-04-29 17:31:38 +0200

    Add Element output

---
+++ src/ElementOutput.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace PTML;
+
+final class ElementOutput
+{
+    public function __construct(
+        private ElementInterface $element
+    ) {
+    }
+
+    /**
+     * Output element to given file path.
+     */
+    public function toFile(string $file_path): bool
+    {
+        return file_put_contents($file_path, $this->element->html()) !== false;
+    }
+}
